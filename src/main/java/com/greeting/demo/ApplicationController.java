0f67304ed62a336b59ac33d4367468360570db50
Dan Bitter <bitter.daniel@gmail.com>
2021-03-03 18:53:28 -0600

    Rename GreetingController to ApplicationController

--- src/main/java/com/greeting/demo/ApplicationController.java
+++ src/main/java/com/greeting/demo/ApplicationController.java
@@ -4,7 +4,7 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-public class GreetingController {
+public class ApplicationController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
