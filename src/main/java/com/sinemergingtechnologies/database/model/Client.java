57762889bab4afdd0b46a17e56831a519a7c30b4
Dan Bitter <bitter.daniel@gmail.com>
2021-03-06 12:41:33 -0600

    Client - implement uuid & BigInt, equals, hashCode

--- src/main/java/com/sinemergingtechnologies/database/model/Client.java
+++ src/main/java/com/sinemergingtechnologies/database/model/Client.java
@@ -3,14 +3,16 @@ package com.sinemergingtechnologies.database.model;
 import lombok.*;
 
 import javax.persistence.*;
+import java.math.BigInteger;
+import java.util.UUID;
 
 @Getter
 @Setter
 @NoArgsConstructor
-//@RequiredArgsConstructor
-@AllArgsConstructor
+@RequiredArgsConstructor
+//@AllArgsConstructor
 @Entity
-@Table(name = "testtable1")
+@Table(name = "clients")
 public class Client {
 
 //    Variable may not have been initialized
@@ -18,8 +20,10 @@ public class Client {
 //    I think this is ok because it runs and Postman works
  // is this needed? rds set up with auto increment
 
-    private @Id @GeneratedValue(strategy = GenerationType.IDENTITY) Long id;
+//    private @Id @GeneratedValue(strategy = GenerationType.IDENTITY) Long id;
 
+    private UUID client_uuid = UUID.randomUUID();
+    private @Id BigInteger id = new BigInteger(client_uuid.toString().replace("-", ""), 16);
     private @NonNull String firstname;
     private @NonNull String lastname;
     private @NonNull String email;
@@ -27,5 +31,28 @@ public class Client {
     private @NonNull String us_state;
     private @NonNull String pass;
     private @NonNull String confirm;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        System.out.println("this != o");
+
+        if (!(o instanceof Client))
+            return false;
+        System.out.println("(o instanceof Client)");
+
+        Client other = (Client) o;
+
+        if (this.getId() == null) return false;
+        System.out.println("this.getId() != null");
+        System.out.println(this.getId() + ":" + other.getId());
+
+        return this.getId().equals(other.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 
 }
