f8b133b36ad84ba286370a9eb4718db00bbe8a41
Dan Bitter <bitter.daniel@gmail.com>
2021-03-06 12:40:55 -0600

    Controller - Move newClient to global for multiple use

--- src/main/java/com/sinemergingtechnologies/database/controller/ApplicationController.java
+++ src/main/java/com/sinemergingtechnologies/database/controller/ApplicationController.java
@@ -1,9 +1,12 @@
 package com.sinemergingtechnologies.database.controller;
 
+import java.math.BigInteger;
 import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.atomic.AtomicLong;
 
 import com.sinemergingtechnologies.database.model.Client;
+import com.sinemergingtechnologies.database.model.Tuple;
 import com.sinemergingtechnologies.database.service.IClientService;
 import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,15 +21,37 @@ public class ApplicationController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    private Client newClient = new Client(
+//            counter.incrementAndGet(),
+            "firstname",
+            "lastname",
+            "email",
+            "city",
+            "us_state",
+            "pass",
+            "confirm"
+    );
+
+
+    private Tuple<UUID, BigInteger> getId() {
+        UUID uuid = UUID.randomUUID();
+        BigInteger bigInt = new BigInteger(uuid.toString().replace("-", ""), 16);
+        return new Tuple<>(uuid, bigInt);
+    }
 //    These paths are case-sensitive it appears
     @GetMapping("/clients")
     public List<Client> getClients(@RequestParam(value = "name", defaultValue = "World") String name) {
         List<Client> clients = (List<Client>) clientService.findAll();
         if (clients.size() > 0) {
-            System.out.println(clients.get(0).toString());
+            Client firstClient = (Client) clients.get(0);
+            System.out.println(firstClient.toString());
+            System.out.println(firstClient.hashCode());
+            System.out.println(firstClient.equals(firstClient));
+            System.out.println(newClient.equals(firstClient));
         } else {
             System.out.println("no clients found");
         }
+        // System.out.println(getId().x);
 
         return clients;
     }
@@ -37,15 +62,7 @@ public class ApplicationController {
     }
     @GetMapping("/testobj")
     public Client testObj(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return new Client(
-                counter.incrementAndGet(),
-                String.format(template, name), //"firstname",
-                "lastname",
-                "email",
-                "city",
-                "us_state",
-                "pass",
-                "confirm"
-        );
+
+        return newClient;
     }
 }
