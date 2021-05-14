35c9de62651b721a813d009fa708d16af43d49b4
Dan Bitter <bitter.daniel@gmail.com>
2021-05-13 22:18:26 -0500

    RolesMapController - return string

--- src/main/java/com/sinemergingtechnologies/database/controller/RolesMapController.java
+++ src/main/java/com/sinemergingtechnologies/database/controller/RolesMapController.java
@@ -1,4 +1,15 @@
 package com.sinemergingtechnologies.database.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/rolesMap")
 public class RolesMapController {
+    @GetMapping("/")
+    public String findRolesMaps() {
+        return "hello";
+    }
 }
